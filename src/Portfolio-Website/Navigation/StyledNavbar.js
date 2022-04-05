9d9e12cea26336c585e3a7eefaa7ccca52da645e
harshita-shetye <100042024+harshita-shetye@users.noreply.github.com>
2022-04-05 23:25:54 +0530

    April 5: Added Sidebar

--- src/Portfolio-Website/Navigation/StyledNavbar.js
+++ src/Portfolio-Website/Navigation/StyledNavbar.js
@@ -1,16 +1,37 @@
-import { useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "./logo.png";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
         </div>
-        <div className="links-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             <li>
               <NavLink
                 to="/"
